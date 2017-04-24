a8e0e5b8543f8cfa1f346d2b5876d359aedcb6f7
Tariq Rafique <mrafique@salesforce.com>
2017-04-23 23:53:38 -0700

    wip

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,7 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using namespace std;
 
 Tools::Tools() {}
 
@@ -11,15 +12,51 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+    cout << "Size mis-match";
+    return rmse;
+  }
+  
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd diff = (estimations[i] - ground_truth[i]).array();
+    diff = diff * diff;
+    rmse = rmse + diff;
+  }
+  
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+  
+  MatrixXd Hj(3,4);
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  
+  //TODO: OPTIMIZE This later
+  float denom = px * px + py * py;
+  if (denom == 0) {
+    return Hj;
+  }
+  //check division by zero
+  
+  //compute the Jacobian matrix
+  Hj <<   px / sqrt(denom), py/sqrt(denom), 0, 0,
+  -1 * py / denom, px/denom, 0, 0,
+  py*(vx*py-vy*px)/pow(denom, 1.5), px*(vy*px-vx*py)/pow(denom, 1.5), px/sqrt(denom), py/sqrt(denom);
+  
+  return Hj;
 }
