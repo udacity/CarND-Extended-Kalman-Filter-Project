95e06715c5c304370b043426f52391da901c2e65
Tom Jacobs <tjacobs@expensify.com>
2017-06-12 17:19:51 +1000

    Add RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,10 +11,30 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	if(estimations.size() == 0 || ground_truth.size() != estimations.size()) {
+	    cout << "Big problems" << endl;
+	    return rmse;
+	}
+
+	// Accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i){
+    	VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse += residual;
+	}
+
+	// Calculate the mean
+	rmse /= estimations.size();
+
+	// Calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	// Return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
