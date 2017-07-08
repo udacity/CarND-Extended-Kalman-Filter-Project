77ba7bb977da4ad29d51f2f793348e3a8509ac02
DIPRE <DIPRECXY@users.noreply.github.com>
2017-07-08 17:29:08 +0800

    up

---
+++ build/CMakeFiles/ExtendedKF.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp" "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/FusionEKF.cpp.o"
+  "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp" "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/kalman_filter.cpp.o"
+  "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/src/main.cpp" "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/main.cpp.o"
+  "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/src/tools.cpp" "/mnt/e/term2/term2-GitHub/CarND-Extended-Kalman-Filter-Project/build/CMakeFiles/ExtendedKF.dir/src/tools.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -65,7 +65,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   VectorXd z_pred(3);
   z_pred << rho, phi, rho_dot;
   VectorXd y = z - z_pred;
-  MatrixXd Ht = H.transpose();
+  MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
