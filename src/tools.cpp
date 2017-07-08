80a6dc4b4aa668b751b0a5935c3401dac36cbc13
DIPRE <DIPRECXY@users.noreply.github.com>
2017-07-08 20:16:33 +0800

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -27,21 +27,20 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	}
 
 	//accumulate squared residuals
-	for(int i=0; i < estimations.size(); ++i){
-      VectorXd residual = estimations[i] - ground_truth[i];
-      residual = residual.array() * residual.array();
-      rmse += residual;
+  for(int i=0; i < estimations.size(); ++i){
+  VectorXd residual = estimations[i] - ground_truth[i];
+  residual = residual.array() * residual.array();
+  rmse += residual;
 		
-	}
+  }
 
 	//calculate the mean
   rmse = rmse/estimations.size();
 	//calculate the squared root
-	rmse = rmse.array().sqrt();
-	// ... your code here
+  rmse = rmse.array().sqrt();
 
 	//return the result
-	return rmse;
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -56,21 +55,21 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   float vx = x_state(2);
   float vy = x_state(3);
   
-	//pre-compute a set of terms to avoid repeated calculation
-	float c1 = px*px+py*py;
-	float c2 = sqrt(c1);
-	float c3 = (c1*c2);
+  //pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
 
-	//check division by zero
-	if(fabs(c1) < 0.0001){
-		std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
-		return Hj;
+  //check division by zero
+  if(fabs(c1) < 0.0001){
+      std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+      return Hj;
 	}
 
-	//compute the Jacobian matrix
-	Hj << (px/c2), (py/c2), 0, 0,
-		  -(py/c1), (px/c1), 0, 0,
-		  py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+  //compute the Jacobian matrix
+  Hj << (px/c2), (py/c2), 0, 0,
+       -(py/c1), (px/c1), 0, 0,
+ 	py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
 
-	return Hj;
+  return Hj;
 }
