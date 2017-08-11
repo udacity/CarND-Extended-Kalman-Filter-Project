b3b2aeec92bfe8959fc4eeadf73d6d05d2b94ea3
scbrubaker02 <scbrubaker02@gmail.com>
2017-08-11 13:47:00 -0400

    Added Project Assistant things

---
+++ .udacity-pa/projects.py
@@ -0,0 +1,26 @@
+import argparse
+from udacity_pa import udacity
+
+nanodegree = 'nd013'
+projects = ['extended-kalman-filter']
+
+def submit(args):
+  parser = argparse.ArgumentParser(description='submits the project.')
+  parser.add_argument('filenames', nargs='*', help='files to include (those included as starter code by default')
+
+  local_args = parser.parse_args(args.args)
+
+  default_filenames = [
+    'src/FusionEKF.cpp',
+    'src/kalman_filter.cpp',
+    'src/tools.cpp'
+  ]
+
+  if len(local_args.filenames) == 0:
+    filenames = default_filenames
+  else:
+    filenames = local_args.filenames
+
+  udacity.submit(nanodegree, projects[0], filenames,
+                 environment = args.environment,
+                 jwt_path = args.jwt_path)
