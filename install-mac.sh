94d0aa6fdc5e2b454776b4ef04e7b8a53ef223d7
BrandonHe <lovinghesl@hotmail.com>
2017-09-08 16:30:51 +0800

    add assignment symbol for variable OPENSSL_LIBRARIES

--- install-mac.sh
+++ install-mac.sh
@@ -8,7 +8,7 @@ mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
 OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
-cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
+cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
 make 
 sudo make install
 cd ..
