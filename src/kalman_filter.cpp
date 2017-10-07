3375bc42d7cd7887acf19b8534d0414ad173ca6c
Andrew Bauman <baumanab@gmail.com>
2017-10-06 23:49:33 -0700

    Add comment regarding Eigen library object initialization

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,6 +3,9 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+// Please note that the Eigen library does not initialize 
+// VectorXd or MatrixXd objects with zeros upon creation.
+
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
