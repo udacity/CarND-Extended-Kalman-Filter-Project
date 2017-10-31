b7d5865b64772ee4753b5414ed247e8476d174f7
anand m <anand@resistance.net>
2017-10-31 14:19:24 -0700

    updated for High Sierra compatibility

--- install-mac.sh
+++ install-mac.sh
@@ -7,8 +7,11 @@ patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
-OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
-cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
+# OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
+# in High Sierra, Apple uses LibreSSL so Homebrew doesn't install OpenSSL binary in /usr/local/bin
+# this should work regardless of which OS version
+OPENSSL_VERSION=`brew info openssl | head -1 | cut -d' ' -f3`
+cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib -DOPENSSL_INCLUDE_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION/include ..
 make 
 sudo make install
 cd ..
