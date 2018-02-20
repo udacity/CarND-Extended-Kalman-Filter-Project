358c5607c04fd123f98761ba02f3990d1b3c7cd6
Kerrick Staley <kerrick@kerrickstaley.com>
2018-02-19 17:24:55 -0800

    Support arch

--- install-linux.sh
+++ install-linux.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git libuv1-dev libssl-dev gcc g++ cmake make
+source /etc/os-release || echo 'Warning: /etc/os-release was not found'
+
+if [[ " $ID_LIKE " =~ ' archlinux ' ]]; then
+  sudo pacman -S git libuv openssl gcc cmake make
+else
+  if [[ ! " $ID_LIKE " =~ ' debian ' ]]; then
+    echo 'Warning: unidentified Linux distribution, assuming Debian-like'
+  fi
+
+  sudo apt-get update
+  sudo apt-get install git libuv1-dev libssl-dev gcc g++ cmake make
+fi
+
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
 git checkout e94b6e1
