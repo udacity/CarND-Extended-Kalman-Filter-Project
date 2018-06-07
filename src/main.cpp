e122c18a9369f404a406125fe8c904b22c0a2de7
Graciela Gonzalez <gmtlali88@gmail.com>
2018-06-06 17:11:36 -0700

    Modifies main.cpp to be able to comunicate with WebGL simulator on workspaces

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "FusionEKF.h"
 #include "tools.h"
 
+
 using namespace std;
 
 // for convenience
@@ -13,17 +14,8 @@ using json = nlohmann::json;
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("]");
-  if (found_null != std::string::npos) {
-    return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
+std::string hasData(std::string s, size_t length) {
+    return s.substr(0, length);
 }
 
 int main()
@@ -38,33 +30,29 @@ int main()
   vector<VectorXd> estimations;
   vector<VectorXd> ground_truth;
 
-  h.onMessage([&fusionEKF,&tools,&estimations,&ground_truth](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
+  h.onMessage([&fusionEKF,&tools,&estimations,&ground_truth](uWS::WebSocket<uWS::SERVER> *ws, char *data, size_t length, uWS::OpCode opCode) {
 
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    if (length && length > 2)
     {
 
-      auto s = hasData(std::string(data));
+      auto s = hasData(std::string(data),length);
       if (s != "") {
-      	
         auto j = json::parse(s);
-
-        std::string event = j[0].get<std::string>();
         
-        if (event == "telemetry") {
           // j[1] is the data JSON object
+          int process = std::stod(j.at("telemetry")["process"].get<std::string>());
+          if(process == 1)
+          {
           
-          string sensor_measurment = j[1]["sensor_measurement"];
-          
-          MeasurementPackage meas_package;
-          istringstream iss(sensor_measurment);
-    	  long long timestamp;
+            string sensor_measurment = j.at("telemetry")["sensor_measurement"];
+              
+            MeasurementPackage meas_package;
+            istringstream iss(sensor_measurment);
+          long long timestamp;
 
-    	  // reads first element from the current line
-    	  string sensor_type;
-    	  iss >> sensor_type;
+          // reads first element from the current line
+          string sensor_type;
+          iss >> sensor_type;
 
     	  if (sensor_type.compare("L") == 0) {
       	  		meas_package.sensor_type_ = MeasurementPackage::LASER;
@@ -104,11 +92,12 @@ int main()
     	  gt_values(2) = vx_gt;
     	  gt_values(3) = vy_gt;
     	  ground_truth.push_back(gt_values);
+
           
-          //Call ProcessMeasurment(meas_package) for Kalman filter
+        //Call ProcessMeasurment(meas_package) for Kalman filter
     	  fusionEKF.ProcessMeasurement(meas_package);    	  
 
-    	  //Push the current estimated x,y positon from the Kalman filter's state vector
+    	  //Push the current estimated x,y position from the Kalman filter's state vector
 
     	  VectorXd estimate(4);
 
@@ -124,7 +113,13 @@ int main()
     	  
     	  estimations.push_back(estimate);
 
+        //Uncomment if neeed  
+    	  //cout << estimations[0] << endl;
+
     	  VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+
+        //Uncomment if neeed
+    	  //cout << RMSE << endl;
 
           json msgJson;
           msgJson["estimate_x"] = p_x;
@@ -133,45 +128,30 @@ int main()
           msgJson["rmse_y"] =  RMSE(1);
           msgJson["rmse_vx"] = RMSE(2);
           msgJson["rmse_vy"] = RMSE(3);
-          auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
-          // std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          auto msg = "{\"process_ekf\"," + msgJson.dump() + "}";
+          ws->send(msg.data(), msg.length(), uWS::OpCode::TEXT);
 	  
         }
-      } else {
+       else {
         
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        std::string msg = "{\"pass\",{}}";
+        ws->send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
+  	}
     }
 
   });
 
-  // We don't need this since we're not using HTTP but if it's removed the program
-  // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-    const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1)
-    {
-      res->end(s.data(), s.length());
-    }
-    else
-    {
-      // i guess this should be done more gracefully?
-      res->end(nullptr, 0);
-    }
-  });
-
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-    ws.close();
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> *ws, int code, char *message, size_t length) {
+    ws->close();
     std::cout << "Disconnected" << std::endl;
   });
 
-  int port = 4567;
+  int port = 3001;
   if (h.listen(port))
   {
     std::cout << "Listening to port " << port << std::endl;
