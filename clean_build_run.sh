032de4aacf94930e497676772828bfd1088506be
andrew bauman <baumanab@gmail.com>
2018-06-11 19:09:01 +0000

    compiles and runs based on workspace or local environment context

--- clean_build_run.sh
+++ clean_build_run.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
+# clean
 cd /home/workspace/CarND-Extended-Kalman-Filter-Project
 rm -rf build
+
+# build
 mkdir build
-./make.sh
-cd build
-./ExtendedKF.out
+
+# compile based on environment {local || workspace}
+if [[ -v WORKSPACEID ]]; then
+	echo compiling workspace main
+    ./make.sh
+    cd build
+    # run workspace version
+    ./ExtendedKF.out
+else 
+	echo  compiling local main
+    cd build
+    cmake .. && make
+    # run local version
+    ./ExtendedKF    
+fi
