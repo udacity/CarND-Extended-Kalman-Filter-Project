a5942bb5d0459dc8e920833cf410854d720ce6fd
andrew <baumanab@gmail.com>
2018-06-13 04:55:54 +0000

    rename main.cpp object files to match local and workspace version names

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
 echo "Compiling main.cpp"
-g++ -o build/ExtendedKF.out src/main.cpp src/FusionEKF.cpp src/kalman_filter.cpp src/tools.cpp -std=c++11 /usr/lib/libuWS.so /usr/lib/x86_64-linux-gnu/libssl.so /usr/lib/x86_64-linux-gnu/libz.so
-
+g++ -o build/ExtendedKF.out src/workspace_main.cpp src/FusionEKF.cpp src/kalman_filter.cpp src/tools.cpp -std=c++11 /usr/lib/libuWS.so /usr/lib/x86_64-linux-gnu/libssl.so /usr/lib/x86_64-linux-gnu/libz.so
