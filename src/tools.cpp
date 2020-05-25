a7095be3080011d6bf1a0542c6120f74fcb22533
Paul Yeh <pyeh.tw@icloud.com>
2020-05-25 18:08:28 +0200

    implement CalculateJacobian()

--- src/tools.cpp
+++ src/tools.cpp
@@ -35,4 +35,32 @@ MatrixXd Tools::CalculateJacobian(const VectorXd &x_state)
    * TODO:
    * Calculate a Jacobian here.
    */
+
+   MatrixXd Hj(3, 4);
+   Hj.fill(0.0);
+   float epslon = 0.00001;
+
+   // recover state parameters
+   float px = x_state(0);
+   float py = x_state(1);
+   float vx = x_state(2);
+   float vy = x_state(3);
+
+   // pre-compute a set of terms to avoid repeated calculation
+   float c1 = px * px + py * py;
+   float c2 = sqrt(c1);
+   float c3 = (c1 * c2);
+
+   // check division by zero
+   if (fabs(c1) < epslon)
+   {
+      return Hj;
+   }
+
+   // compute the Jacobian matrix
+   Hj << (px / c2), (py / c2), 0, 0,
+       -(py / c1), (px / c1), 0, 0,
+       py * (vx * py - vy * px) / c3, px * (px * vy - py * vx) / c3, px / c2, py / c2;
+
+   return Hj;
 }
